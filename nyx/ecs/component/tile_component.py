86359b9b703e55fa93bc6d2471358154beb5ee78
Charles Morman <github@cmorman.com>
2024-12-04 20:41:31 -0700

    feat: add `TileMapComponent` ref #4

--- nyx/ecs/component/tile_component.py
+++ nyx/ecs/component/tile_component.py
@@ -17,3 +17,13 @@ class GraphicComponent(NyxComponent):
         if not isinstance(graphic, np.ndarray) or graphic.dtype != np.uint8:
             raise ValueError("Graphic must be a numpy ndarray of dtype 'uint8'")
         self.graphic = graphic
+
+
+class TileMapComponent(NyxComponent):
+    def __init__(self, tilemap: np.ndarray, tile_width: int = 1, tile_height: int = 1):
+        if not isinstance(tilemap, np.ndarray) or tilemap.dtype != np.uint8:
+            raise ValueError("Tilemap must be a NumPy `ndarray` of `dtype` 'uint8'")
+
+        self.tilemap = tilemap
+        self.tile_width = tile_width
+        self.tile_height - tile_height
