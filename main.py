3ce6bd0e87ef56f3096f3fde035f98f114f127c0
Charles Morman <github@cmorman.com>
2024-12-08 01:19:33 -0700

    feat: recreate the entity and entity manager classes ref #1, #4, #6

--- main.py
+++ main.py
@@ -1,37 +1,30 @@
-import time
-import numpy as np
-from nyx.ecs.component.components import GraphicComponent, TransformComponent
-from nyx.ecs.nyx_entity_manager import NyxEntityManager
-from nyx.ecs.system.render_system import RenderSystem
+from nyx.aether.aether_compositor import AetherCompositor
+from nyx.engine.ecs.component.nyx_component import (
+    BackgroundColorComponent,
+    ZIndexComponent,
+)
+from nyx.engine.ecs.component.nyx_component_store import NyxComponentStore
+from nyx.engine.ecs.entity.nyx_entity_manager import NyxEntityManager
+from nyx.engine.ecs.system.render_system import AetherBridgeSystem
+
 
 if __name__ == "__main__":
-    nyx_ecs = NyxEntityManager()
-    renderer = RenderSystem(view_width=100, view_height=40, manager=nyx_ecs)
-    RenderSystem.clear_terminal()
-    while True:
-        graphic_1 = np.random.randint(low=196, high=201, size=(16, 16), dtype=np.uint8)
-        tile_1 = (
-            nyx_ecs.create_entity("random-tile-colors")
-            .add_component(TransformComponent(0, 0))
-            .add_component(GraphicComponent(graphic_1))
-        )
-        graphic_3 = np.random.randint(low=22, high=51, size=(32, 32), dtype=np.uint8)
-        tile_3 = (
-            nyx_ecs.create_entity("random-tile-colors")
-            .add_component(TransformComponent(100 - 96, 40 - 32))
-            .add_component(GraphicComponent(graphic_3))
-        )
-        graphic_2 = np.random.randint(low=196, high=225, size=(8, 8), dtype=np.uint8)
-        tile_2 = (
-            nyx_ecs.create_entity("random-tile-colors")
-            .add_component(TransformComponent(50 - 12, 20 - 4))
-            .add_component(GraphicComponent(graphic_2))
-        )
+    # Load Early Managers, Stores, Assets, Systems
+    entity_manager = NyxEntityManager()
+    aether_compositor = AetherCompositor()
+    component_store = NyxComponentStore(entity_manager)
+    render_system = AetherBridgeSystem(
+        entity_manager, component_store, aether_compositor
+    )
 
-        renderer.render(clear_term=False)
-        print(nyx_ecs)
-        print(type[tile_1])
-        print("Unloading assets:")
-        nyx_ecs.unload_entity(tile_1).unload_entity(tile_2).unload_entity(tile_3)
-        print(nyx_ecs)
-        time.sleep(0.1)
+    # Create "LevelRoot" Entity
+    level_root_entity = entity_manager.create_entity("LevelRoot")
+
+    # Add components to entity
+    component_store.register_component_to_entity(
+        level_root_entity, BackgroundColorComponent(33)
+    ).register_component_to_entity(level_root_entity, ZIndexComponent(0))
+
+    render_system.update()
+
+    print(aether_compositor.layers)
