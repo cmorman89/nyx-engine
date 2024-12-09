8833db2e8b7704119c75a5145d5850fb195c11ed
Charles Morman <github@cmorman.com>
2024-12-09 03:20:14 -0700

    feat: implement tilemap rendering in `AetherRenderer` ref #5, #6, #7, #8

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+from random import randint
+import numpy as np
 from nyx.aether.aether_renderer import AetherRenderer
 from nyx.engine.ecs.component.renderable_components import (
     BackgroundColorComponent,
+    TilemapComponent,
     ZIndexComponent,
 )
 from nyx.engine.ecs.component.scene_component import SceneComponent
 from nyx.engine.stores.nyx_component_store import NyxComponentStore
 from nyx.engine.managers.nyx_entity_manager import NyxEntityManager
-from nyx.engine.ecs.system.render_system import AetherBridgeSystem
+from nyx.engine.ecs.system.aether_bridge_system import AetherBridgeSystem
+from nyx.engine.stores.tileset_store import TilesetStore
 from nyx.hemera.hemera_term_fx import HemeraTermFx
 
 
@@ -18,26 +22,37 @@ if __name__ == "__main__":
     component_store = NyxComponentStore(entity_manager)
     aether_collector = AetherBridgeSystem(entity_manager, component_store)
 
+    # Create a simple tile map and tiles
+    tilemap = np.array([[1, 2, 1, 2], [4, 3, 2, 1], [0, 0, 0, 0]], dtype=np.uint8)
+    tileset_store = TilesetStore()
+    tile_size = (16, 16)
+    for i in range(5):
+        tile_texture = np.full(tile_size, randint(15, 255), dtype=np.uint8)
+        tile_friendly_name = f"tile-{i}"
+        tileset_store.create_tile(
+            tile_texture=tile_texture, tile_id=i, tile_friendly_name=tile_friendly_name
+        )
+
     # Create "LevelRoot" Entity
-    level_root_entity = entity_manager.create_entity("LevelRoot")
+    scene_entity = entity_manager.create_entity("scene-1")
 
     # Add components to entity
     (
         component_store.register_entity_component(
-            level_root_entity, SceneComponent("Demo")
+            scene_entity, SceneComponent("demo-map")
         )
-        .register_entity_component(level_root_entity, ZIndexComponent(0))
-        .register_entity_component(level_root_entity, BackgroundColorComponent(33))
+        .register_entity_component(scene_entity, ZIndexComponent(0))
+        .register_entity_component(scene_entity, BackgroundColorComponent(33))
+        .register_entity_component(scene_entity, TilemapComponent(tilemap))
     )
 
     # Call the render system to collect entities and pass to Aether
     z_indexed_entities = aether_collector.update()
 
-    aether_renderer.view_h = 16
-    aether_renderer.view_w = 16
-    hemera_term_api.output(
-        aether_renderer.accept_entities(aether_collector.update()).render()
-    )
+    aether_renderer.view_h = 25
+    aether_renderer.view_w = 25
+    merged_frame = aether_renderer.accept_entities(aether_collector.update()).render()
+    hemera_term_api.output(merged_frame)
 
     # rendered_frame = aether_renderer.render()
 
--- nyx/aether/aether_renderer.py
+++ nyx/aether/aether_renderer.py
@@ -1,3 +1,4 @@
+from math import ceil
 from typing import Dict
 from uuid import UUID
 
@@ -8,6 +9,7 @@ from nyx.engine.ecs.component.renderable_components import (
     RenderableComponent,
     TilemapComponent,
 )
+from nyx.engine.stores.tileset_store import TilesetStore
 from nyx.hemera.term_utils import TerminalUtils
 
 
@@ -29,10 +31,19 @@ class AetherRenderer:
     def accept_entities(
         self, entities: Dict[int, Dict[UUID, Dict[str, RenderableComponent]]]
     ):
+        """Receive and store the list of entities to render from AetherBridgeSystem
+
+        Args:
+            entities (Dict[int, Dict[UUID, Dict[str, RenderableComponent]]]): _description_
+
+        Returns:
+            _type_: _description_
+        """
         self.z_indexed_entity_layers = entities
         return self
 
     def render(self):
+        """Main entry point for rendering"""
         if not self.z_indexed_entity_layers:
             raise ValueError("AetherRenderer has no layers to render.")
         self.z_indexed_working_frames = {}
@@ -57,7 +68,7 @@ class AetherRenderer:
                     if z_index == 0 and isinstance(component, BackgroundColorComponent):
                         self._process_background_color_component(component)
                     elif isinstance(component, TilemapComponent):
-                        pass  # in progress
+                        self._process_tilemap_component(component)
 
     def _new_subframe(self, z_index: int = 0):
         """Create a new working frame for each z-index/priority/layer."""
@@ -93,3 +104,28 @@ class AetherRenderer:
     ):
         """Store the background color for later use."""
         self.background_color_code = bg_component.bg_color_code
+
+    def _process_tilemap_component(self, component: TilemapComponent):
+        tilemap = component.tilemap
+        textures = TilesetStore.tileset_textures
+        map_h, map_w = tilemap.shape
+        tile_d = component.tile_dimension
+        rendered_map = self.z_indexed_working_frames[
+            0
+        ]  # Tilemap exclusively occupies layer 0
+        # Render 1 tile out of visible in x and y if any of that tile showing in the view.
+        # map_w_max = ceil(self.view_w / tile_d)
+        # map_h_max = ceil(self.view_h / tile_d)
+        # But for now, lets not exceed the bounds:
+        map_w_max = self.view_w // tile_d
+        map_h_max = self.view_h // tile_d
+        for y in range(min(map_h, map_h_max)):
+            for x in range(min(map_w, map_w_max)):
+                # Get the tile id at location y, x.
+                tile_id = tilemap[y, x]
+                # Get the texture for that tile ID.
+                tile_texture = textures[tile_id]
+                # Slice the ndarray from y, x (origin) for d elements in each axis.
+                rendered_map[
+                    y * tile_d : (y + 1) * tile_d, x * tile_d : (x + 1) * tile_d
+                ] = tile_texture
