66f7f111a051e02074bc270e75db6a1257814886
Charles Morman <github@cmorman.com>
2024-12-09 01:57:19 -0700

    feat: add `TilemapComponent` to hold the tile map ndarray ref #5

--- nyx/engine/ecs/component/renderable_components.py
+++ nyx/engine/ecs/component/renderable_components.py
@@ -1,4 +1,6 @@
 from abc import ABC
+
+import numpy as np
 from nyx.engine.ecs.component.nyx_component import NyxComponent
 
 
@@ -28,8 +30,22 @@ class ZIndexComponent(RenderableComponent):
         self.z_index: int = z_index
 
 
+class TilemapComponent(NyxComponent):
+    """Holds the 2D NumPy array of tile IDs that compose the tilemap"""
+
+    def __init__(self, tilemap: np.ndarray):
+        if not isinstance(tilemap, np.ndarray) or tilemap.dtype != np.uint8:
+            raise ValueError("Tilemap must be a NumPy `ndarray` of `dtype` 'uint8'")
+
+        self.tilemap = tilemap
+
+    def __str__(self):
+        """Return the class name (which is same as component name) when called as string."""
+        return f"{type(self).__name__}"
+
+
 # class TransformComponent(NyxComponent):
-#     def __init__(self, x: int, y: int):
+#     def __init__(self, x: int, y: int):1
 #         self.x = x
 #         self.y = y
 
@@ -41,18 +57,3 @@ class ZIndexComponent(RenderableComponent):
 #         if not isinstance(graphic_arr, np.ndarray) or graphic_arr.dtype != np.uint8:
 #             raise ValueError("Graphic must be a numpy ndarray of dtype 'uint8'")
 #         self.graphic_arr = graphic_arr
-
-
-# class TilemapComponent(NyxComponent):
-#     """Holds the 2D NumPy array of tile IDs"""
-
-#     def __init__(self, tilemap: np.ndarray, tilemap_bg: int = None):
-#         if not isinstance(tilemap, np.ndarray) or tilemap.dtype != np.uint8:
-#             raise ValueError("Tilemap must be a NumPy `ndarray` of `dtype` 'uint8'")
-
-#         self.tilemap_arr = tilemap
-#         self.tilemap_bg: int = tilemap_bg
-
-#     def __str__(self):
-#         """Return the class name (which is same as component name) when called as string."""
-#         return f"{Type[self]}"
