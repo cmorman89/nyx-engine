493131bea6b9e41d8a3a5b8c9b2b902a22507179
Charles Morman <github@cmorman.com>
2024-12-10 00:40:23 -0700

    feat: add subpixel ANSI cursor relocation with `cursor_subpixel_abs_move` method

--- nyx/hemera/term_utils.py
+++ nyx/hemera/term_utils.py
@@ -1,5 +1,4 @@
 import os
-from re import A
 
 
 class TerminalUtils:
@@ -37,6 +36,22 @@ class TerminalUtils:
     @staticmethod
     def cursor_abs_move(row: int, col: int):
         return f"\033[{row};{col}H"
+
+    @staticmethod
+    def cursor_subpixel_abs_move(pixel_x: int, sub_pixel_y: int):
+        """Account for each character being composed of two stacked pixels.
+
+        Args:
+            pixel_x (int): the x index of the pixel coumn
+            sub_pixel_y (int): the y index based on the subpixel row
+
+        Returns:
+            str: ANSI escape sequence to relocate the cursor.
+        """
+        # Convert zero-based indices to one-based for ANSI
+        row = (sub_pixel_y // 2) + 1
+        column = pixel_x + 1
+        return f"\033[{row};{column}H"
 
     @staticmethod
     def get_terminal_dimensions():
