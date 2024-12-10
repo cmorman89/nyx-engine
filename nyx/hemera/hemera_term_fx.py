633d9fa90de1ce644718d1a2e17a5df5108f40ee
Charles Morman <github@cmorman.com>
2024-12-09 23:11:40 -0700

    feat: add delta framebuffering; regression in display until cursor relocation also implemented

--- nyx/hemera/hemera_term_fx.py
+++ nyx/hemera/hemera_term_fx.py
@@ -5,12 +5,15 @@ from nyx.hemera.term_utils import TerminalUtils
 
 
 class HemeraTermFx:
-    def __init__(self, viewport_h: int = 0, viewport_w: int = 0):
+    def __init__(self, clear_term_on_run: bool = False):
         self._ansi_table = self._precompute_ansi_table()
+        self.old_frame = None
+        self.clear_term_on_run = clear_term_on_run
 
     def output(self, new_frame: np.ndarray = None):
+        self.print_subpixel_to_term(self.generate_delta_framebuffer(new_frame))
         # self.print_to_term(new_frame)
-        self.print_subpixel_to_term(new_frame)
+        # self.print_subpixel_to_term(new_frame)
 
     def print_to_term(self, new_frame):
         frame = new_frame
@@ -55,3 +58,17 @@ class HemeraTermFx:
             [f"\033[38;5;{color}m██" if color > 0 else "  " for color in ansi_range],
             dtype="<U20",
         )
+
+    def generate_delta_framebuffer(self, new_frame):
+        # Generate a new, blank historical frame if one does not exist or term dimensions change.
+        # This will force a redraw of the whole scene
+        if self.old_frame is None or self.old_frame.shape != new_frame.shape:
+            self.old_frame = np.zeros(new_frame.shape, dtype=np.uint8)
+
+        # Create the delta framebuffer by:
+        # - Compare the new frame to the old frame
+        # - Keep the changed pixels, and set the rest to 0.
+        # - "0" represents an *unchanged* pixel that does not need to be redrawn.
+        delta_framebuffer = np.where(new_frame != self.old_frame, new_frame, 0)
+        self.old_frame = new_frame
+        return delta_framebuffer
