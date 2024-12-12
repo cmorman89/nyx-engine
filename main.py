dd1399ec0867a3e132b40717646aeda9c285e399
Charles Morman <github@cmorman.com>
2024-12-11 22:31:37 -0700

    feat: update `main` demo to use the example tiles and the subpixel, buffered renderer.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from pprint import pprint
+
 from random import randint
 import time
+from typing import Dict, List
 import numpy as np
 from nyx.aether.aether_renderer import AetherRenderer
 from nyx.engine.ecs.component.renderable_components import (
@@ -27,33 +30,38 @@ if __name__ == "__main__":
 
     # Create a simple tile map and tiles
     tilemap = np.array([[0, 2, 1, 2], [4, 3, 0, 1], [0, 1, 0, 1]], dtype=np.uint8)
-    tile_dimension = 24
+    tile_dimension = 32
     tileset_store = TilesetStore()
     tile_size = (tile_dimension, tile_dimension)
-    tile_load = NyxAssetImport.open_asset("spaceship")
+
+    # Load tiles from .nyx files
+    tile_files = [f"space-tiles-{i}" for i in range(1, 17)]
+    # Save them to a temporary dictionary
+    tile_imports: Dict[int, np.ndarray] = {}
+    for i, filename in enumerate(tile_files):
+        tile = NyxAssetImport.open_asset(filename)
+        tile_imports[i] = tile
+    for i, tile_texture in enumerate(tile_imports):
+        # tile_texture = np.full(tile_size, randint(15, 255), dtype=np.uint8)
+        tile_texture = tile_imports.get(i)
+        tile_friendly_name = f"tile-{i}"
+        tileset_store.create_tile(
+            tile_texture=tile_texture,
+            tile_id=i,
+            tile_friendly_name=tile_friendly_name,
+            overwrite=True,
+        )
+    # pprint(tile_files)
+    # pprint(tile_imports)
+    # pprint(tileset_store.tileset_textures)
+    # input("Enter to continue")
 
     # Clear the terminal before the first run
     TerminalUtils.clear_term()
 
     # Loop to regenerate colors
     while True:
-        TilesetStore.reset_store()
-        tileset_store.create_tile(
-            tile_texture=tile_load,
-            tile_id=0,
-            tile_friendly_name="spaceship",
-            overwrite=True,
-        )
-        for i in range(1, 5):
-            tile_texture = np.full(tile_size, randint(15, 255), dtype=np.uint8)
-            tile_friendly_name = f"tile-{i}"
-            tileset_store.create_tile(
-                tile_texture=tile_texture,
-                tile_id=i,
-                tile_friendly_name=tile_friendly_name,
-                overwrite=True,
-            )
-
+        tilemap = np.random.randint(1, 16, size=(10, 20), dtype=np.uint8)
         # Create "LevelRoot" Entity
         scene_entity = entity_manager.create_entity("scene-1")
 
@@ -63,7 +71,8 @@ if __name__ == "__main__":
                 scene_entity, SceneComponent("demo-map")
             )
             .register_entity_component(scene_entity, ZIndexComponent(0))
-            .register_entity_component(scene_entity, BackgroundColorComponent(33))
+            # .register_entity_component(scene_entity, BackgroundColorComponent(33))
+            .register_entity_component(scene_entity, BackgroundColorComponent(16))
             .register_entity_component(
                 scene_entity, TilemapComponent(tilemap, tile_dimension=tile_dimension)
             )
@@ -72,15 +81,17 @@ if __name__ == "__main__":
         # Call the render system to collect entities and pass to Aether
         z_indexed_entities = aether_collector.update()
 
-        # aether_renderer.view_h = 25
-        # aether_renderer.view_w = 25
+        aether_renderer.viewport_h = 224
+        aether_renderer.viewport_w = 228
         merged_frame = aether_renderer.accept_entities(
             aether_collector.update()
         ).render()
         hemera_term_api.output(merged_frame)
         # rendered_frame = aether_renderer.render()
         # hemera_term_api.output(rendered_frame)
+        # time.sleep(2)
+        time.sleep(0.5)
         # time.sleep(0.0666) # 15 fps
-        # time.sleep(0.0333) # 30 fps
+        # time.sleep(0.0333)  # 30 fps
         # time.sleep(0.0167) # 60 fps
-        time.sleep(0.00833) # 120 FPS
+        # time.sleep(0.00833)  # 120 FPS
