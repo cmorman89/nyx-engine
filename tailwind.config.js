a414458a01e67b53cd3a49e77d1155b76e8c0dfb
Charles Morman <github@cmorman.com>
2025-01-02 13:20:40 -0700

    feat: use new home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
         nyx: '#00002B',
         nyxDarkPurpleBlue: '#1a0751',
         nyxDarkPurple: '#19021B',
-        // header_purple: '#190432',
+        nyxNavBlackTop: '#0D0D0D',
+        nyxNavBlackBottom: '#18001C',
         nyxTerminalGreen: '#45CD45'
       },
       fontFamily: {
@@ -20,6 +21,9 @@ module.exports = {
       },
       lineHeight: {
         hero: '1.40',
+      },
+      boxShadow: {
+        pinkGlow: '0 8px 40px 0 rgba(158, 4, 172, 0.25)'
       },
     },
   },
