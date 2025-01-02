16c9afb4c39a0725fd07bc5ecdf76af83c2d39a0
Charles Morman <github@cmorman.com>
2025-01-02 10:29:05 -0700

    feat: starting v2 of nyx engine site

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,24 @@
 module.exports = {
   content: [
     './index.html',
+    './index_2.html',
   ],
   theme: {
     extend: {
       colors: {
         nyx: '#00002B',
+        nyxDarkPurpleBlue: '#1a0751',
+        nyxDarkPurple: '#19021B',
+        // header_purple: '#190432',
+        nyxTerminalGreen: '#45CD45'
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        pixelify: ['Pixelify Sans', 'sans-serif'],
+        ptSansNarrow: ['PT Sans Narrow', 'sans-serif'],
+      },
+      lineHeight: {
+        hero: '1.20',
       },
     },
   },
